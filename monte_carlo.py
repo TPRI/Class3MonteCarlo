be7394dd30e7dde945ee149209a40590008dbb69
TPRI <timothy.rose-innes.14@ucl.ac.uk>
2014-10-23 22:59:50 +0100

    Added more pseudocode and added method declarations

--- monte_carlo.py
+++ monte_carlo.py
@@ -1,14 +1,37 @@
 __author__ = 'Timothy Rose-Innes'
 
-# Sketch of Monte Carlo Sim Program
+class monte_carlo(object):
 
-#Class: Monte Carlo Sim
+    def __init__(self,temp):
+        self.temp = temp
 
-#Constructor ??
+    # Randomly move a particle
+    def random_move(self,density):
+        # Count n particles
+        #   Sum over all particles in density
+        # Select particle at random
+        #   Select random number 0 to n
+        # Count over particles up to n until reach correct element
+        # Subtract 1 from element
+        # Add 1 to left/right element
+        return density_new
 
-#Method: Move random
+    # Compare initial and final energies
+    def compare_energies(self, density, density_new, energy):
+        # if energy lower
+        #   return true
+        # else
+        #   return false
 
-#Method: Compare Energies
+    #Method Compare Boltzmann Factor
+    def compare_boltzmann_factor(self, density, density_new, energy):
+    # Calculate Boltzmann factor P1 between E0 and E1
+    # Compare to a random number P0 ??? What should the range be ???
+    # if P0 > P1
+	#   return true
+    # else
+    #   return false
+
 
 #Method: Main (energy, density, iterations, temperature)
    #Create Monte Carlo Sim Object
