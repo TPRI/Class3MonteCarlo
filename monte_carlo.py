d4d4f342ceaca8944d45853413912afa6df6d756
TPRI <timothy.rose-innes.14@ucl.ac.uk>
2014-10-23 22:30:44 +0100

    Sketch of Monte Carlo Program

---
+++ monte_carlo.py
@@ -0,0 +1,19 @@
+__author__ = 'Timothy Rose-Innes'
+
+# Sketch of Monte Carlo Sim Program
+
+#Class: Monte Carlo Sim
+
+#Constructor ??
+
+#Method: Move random
+
+#Method: Compare Energies
+
+#Method: Main (energy, density, iterations, temperature)
+   #Create Monte Carlo Sim Object
+   #Compute E0
+   #Call move random
+   #Compute E1
+   #Call Compare Energies
+   #Iterate
