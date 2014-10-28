d5d69505151b8c46b9d3abb443f6c2cef8389332
TPRI <timothy.rose-innes.14@ucl.ac.uk>
2014-10-28 21:06:01 +0000

    More comments in ExampleCall

--- ExampleCall.py
+++ ExampleCall.py
@@ -1,9 +1,14 @@
 __author__ = 'Timothy Rose-Innes'
 
+""" An example of calling an instance of the MonteCarlo class """
+
 from monte_carlo import MonteCarlo
 from diffusion_model import energy
 from numpy.random import random_integers
 
+# Create an instance, mc, of the MonteCarlo class
 mc = MonteCarlo(1,1000)
+
+# Call the instance mc 
 mc(random_integers(100, size=10), energy)
 
