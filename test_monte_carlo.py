57db87d9a491b370e511abb2f2efc2e10a5d1cb2
TPRI <timothy.rose-innes.14@ucl.ac.uk>
2014-10-28 17:52:21 +0000

    Tided up references to MonteCarlo class and added TODOs

--- test_monte_carlo.py
+++ test_monte_carlo.py
@@ -1,32 +1,33 @@
-from monte_carlo import monte_carlo
+from monte_carlo import MonteCarlo
 from nose.tools import assert_equal, assert_raises, assert_true, assert_false
 from numpy.random import random_integers
 
 #random_move tests
 
 def test_fails_for_negative_densities():
-    mc = monte_carlo()
+    mc = MonteCarlo()
     with assert_raises(ValueError) as exception: mc.random_move([-1, 2, 3, 4])
 
 def test_fails_for_non_integer_densities():
-    mc = monte_carlo()
+    mc = MonteCarlo()
     with assert_raises(ValueError) as exception: mc.random_move([1.0, 2, 3, 4])
 
 def test_handles_zero_densities():
-    mc = monte_carlo()
+    mc = MonteCarlo()
     densities = [[],[0],[0,0,0,0]]
     for density in densities:
        with assert_raises(ValueError) as exception: mc.random_move(density)
 
 def test_particle_number_is_conserved():
-    mc = monte_carlo()
+    mc = MonteCarlo()
     density = random_integers(100, size=100)
     n = sum(density)
     n_new = sum(mc.random_move(density))
     assert_equal(n,n_new,"particle number not consevered")
 
-# def one_particle_is_moved():
-#     mc = monte_carlo()
+# TODO review test_one_particle_is_moved
+# def test_one_particle_is_moved():
+#     mc = MonteCarlo()
 #     density = random_integers(100, size=10)
 #     density_new = mc.random_move(density)
 #     difference = density - density_new
@@ -35,7 +36,7 @@ def test_particle_number_is_conserved():
 # compare_energy tests
 
 def test_compare_energies():
-    mc = monte_carlo()
+    mc = MonteCarlo()
     assert_true(mc.compare_energy(2,1))
     assert_false(mc.compare_energy(1,2))
     assert_false(mc.compare_energy(1,1))
@@ -43,9 +44,11 @@ def test_compare_energies():
 # compare_boltzmann_factor tests
 
 def test_compare_boltzman_factor_equal():
-    mc = monte_carlo()
+    mc = MonteCarlo()
     assert_true(mc.compare_boltzmann_factor(1,1))
 
 def test_compare_boltzman_factor_high_t():
-    mc = monte_carlo(10000000000)
+    mc = MonteCarlo(10000000000)
     assert_true(mc.compare_boltzmann_factor(100,1))
+
+#TODO main algorithm tests
