0ed3c93635eaaa21ad7067910f8225f539f786b5
TPRI <timothy.rose-innes.14@ucl.ac.uk>
2014-10-28 15:52:53 +0000

    Added to diffusion_model further TypeErrors and ValueErrors

--- diffusion_model.py
+++ diffusion_model.py
@@ -1,14 +1,19 @@
 def energy(density, coeff=1.0):
 
-  #Imports
-  from numpy import array, sum
+    #Imports
+    from numpy import array, sum
   
-  #Assign density list to an numpy array
-  density = array(density)
+    #Assign density list to an numpy array
+    density = array(density)
   
-  if density.ndim != 1:
-    raise ValueError("Density should be an a *1-dimensional* array of positive integers.")
-  
-  #Main code in return 
-  return coeff * sum(density * (density - 1)) / 2
-  
+    if density.ndim != 1:
+        raise ValueError("Density should be an a *1-dimensional* array.")
+
+    if any(density < 0):
+        raise ValueError("Density should be an array of *positive integers*.")
+
+    if density.dtype.kind != 'i' and len(density) > 0:
+        raise TypeError("Density should be an array of *integers*.")
+
+    #Main code in return
+    return coeff * sum(density * (density - 1)) / 2
