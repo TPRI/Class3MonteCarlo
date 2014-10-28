cd798242738be42121000728640773cd5a70ef55
TPRI <timothy.rose-innes.14@ucl.ac.uk>
2014-10-28 15:51:47 +0000

    Ammended energy tests

--- test_diffusion_model.py
+++ test_diffusion_model.py
@@ -4,6 +4,7 @@ from diffusion_model import energy
 
 def test_energy_fails_for_non_integer_density():
     with assert_raises(TypeError) as exception: energy([1.1, 2, 3])
+    with assert_raises(TypeError) as exception: energy([1.0, 2, 3])
 
 def test_energy_fails_negative_density():
     with assert_raises(ValueError) as exception: energy([-1, 2, 3])
@@ -13,7 +14,8 @@ def test_energy_fails_density_wrong_dimension():
 
 def test_zero_density():
     densities = [[], [0], [0, 0, 0]]
-    for density in densities: assert_almost_equal(energy(density), 0)
+    for density in densities:
+        assert_almost_equal(energy(density), 0)
 
 def test_zero_coeff():
     assert_almost_equal(energy([1, 1, 1], coeff=0), 0)
